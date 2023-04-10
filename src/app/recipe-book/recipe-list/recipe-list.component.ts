f890a6ff896d415c079fe82572de1d9e0523a780
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-04-10 12:18:48 -0300

    recipe

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent {
       'Teste receita',
       'Uma descrição bem daora',
       'https://images.pexels.com/photos/4551832/pexels-photo-4551832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    ),
+    new Recipe(
+      'Teste receita2',
+      'Uma descrição bem daora2',
+      'https://images.pexels.com/photos/4551832/pexels-photo-4551832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     )
   ];
 }
