d6c92ee43057243fcd8f72ec1587ccd82f171f38
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-04-14 11:59:22 -0300

    recipe

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'Teste receita',
@@ -19,4 +21,8 @@ export class RecipeListComponent {
       'https://images.pexels.com/photos/4551832/pexels-photo-4551832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     )
   ];
+
+  onRecipeClicked(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
