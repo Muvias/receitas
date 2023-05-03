ab3270f2ffd9b5d928523c4f70cd59de19ecfa76
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-05-03 13:45:16 -0300

    recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { DropdownDirective } from './shared/dropdown.directive';
 
@@ -38,6 +38,7 @@ import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.compo
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRouterModule
   ],
