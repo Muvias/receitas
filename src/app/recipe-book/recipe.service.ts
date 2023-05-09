9d12c3bb99c72e032f7ede60d1e21c2a144c154a
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-05-09 12:12:04 -0300

    recipe

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -9,28 +9,14 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Teste receita',
-      'Uma descrição bem daora',
-      'https://images.pexels.com/photos/4551832/pexels-photo-4551832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      [
-        new Ingredient('Carne', 1),
-        new Ingredient('Batata Frita', 20),
-      ]
-    ),
-    new Recipe(
-      'Teste receita2',
-      'Uma descrição bem daora2',
-      'https://images.pexels.com/photos/4551832/pexels-photo-4551832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      [
-        new Ingredient('Pão de queijo', 5),
-        new Ingredient('Manteiga', 1),
-      ]
-    )
-  ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {};
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
 
   getRecipesArray() {
     return this.recipes.slice();
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+import { RecipeService } from "../recipe-book/recipe.service";
+import { Recipe } from "../recipe-book/recipe.model";
+
+@Injectable({ providedIn: "root" })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipesService: RecipeService) { };
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipesArray();
+    this.http.put('https://ng-receps-default-rtdb.firebaseio.com/recipes.json', recipes)
+      .subscribe(response => console.log(response));
+  };
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-receps-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          }
+        });
+      }))
+      .subscribe(recipes => this.recipesService.setRecipes(recipes));
+  }
+};
