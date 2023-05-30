47ef4efa6821ed6023721a1d6740d595b0d6d676
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-05-30 13:25:44 -0300

    add reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { StoreModule } from '@ngrx/store';
+
 import { AppRouterModule } from './app-router.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -21,6 +25,7 @@ import { CoreModule } from './core.module';
     AppRouterModule,
     SharedModule,
     CoreModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
   ],
   bootstrap: [AppComponent]
 })
