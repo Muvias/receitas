07329015ec81abcc0649644889538438bef63fe3
Vinicius Perazza <vi_guepardo@hotmail.com>
2023-06-02 15:53:47 -0300

    edit mode changed

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,6 +6,7 @@ import { ShoppingListService } from './shopping-list.service';
 import { Store } from '@ngrx/store';
 
 import * as fromShoppingList from './store/shopping-list.reducer';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
@@ -25,6 +26,6 @@ export class ShoppingListComponent implements OnInit {
   };
 
   onEditItem(index: number) {
-    this.shoppingListService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index))
   };
 }
